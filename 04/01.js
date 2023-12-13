dda06fdd1d8681f49619d1c8b963fdcb6f7da5b2
Eric Ritchey <eric@eandjdesign.com>
2023-12-13 02:24:25 +0000

    Day 4, Part 1. A lot less code than i thought it'd have to be.

---
+++ 04/01.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const { machine } = require('os');
+
+const IS_TEST = false
+
+// turn the data into an array
+if (IS_TEST) {
+  // very simple test
+  // fileName = 'simple.txt'
+
+  // advent's test data
+  fileName = 'test.txt'
+} else {
+  fileName = 'input.txt'
+}
+
+dataFile = `${__dirname}/data/${fileName}`
+
+/**
+ * Step 1: Injest the data
+ */
+const datum = fs.readFileSync(dataFile, 'utf8')
+const dataset = datum.split('\n')
+
+/**
+ * Step 2: Split the data up into their parts
+*/
+
+let setTotal = 0;
+
+for (let card of dataset) {
+  let cardTotal = 0;
+  const regex = /(.*):(.*)\|(.*)/g;
+  const [scratchCard] = card.matchAll(regex)
+  
+  title = scratchCard[1];
+  wins  = ' ' + scratchCard[2] + ' ';
+  mine  = ' ' + scratchCard[3] + ' ';
+
+  // make an array and remove all the blanks
+  const myNumbers = mine.split(' ').filter(e => e)
+
+  // loop through myNumbers, adding blank spaces before/after the current item so we don't mismatch. 
+  // if current item is in the wins string, perform maths against it.
+  myNumbers.forEach(item => {
+    scratched = ' ' + item + ' ';
+    if(wins.includes(scratched)) cardTotal = cardTotal == 0 ? 1 : cardTotal * 2
+  });
+  // console.log(`${title}: ${cardTotal}`)
+
+  setTotal+=cardTotal;
+
+}
+
+console.log(`Total: ${setTotal}`);
